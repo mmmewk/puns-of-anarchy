30c66818e52b1ee968c32f7628ee4e1b06a0ede3
Matthew Koppe <matthew@ever.green>
2023-09-10 19:23:48 -0700

    Add: game lobby

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import PunsOfAnarchy from './PunsOfAnarchy';
 import { ConfigProvider } from 'antd';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Lobby } from 'boardgame.io/react';
+import PunsOfAnarchyGame from './PunsOfAnarchy/Game';
+import PunsOfAnarchyBoard from './PunsOfAnarchy/Board';
+
+const server = `https://yourapplication.herokuapp.com`;
+const importedGames = [{ game: PunsOfAnarchyGame, board: PunsOfAnarchyBoard }];
 
 const App : React.FC = () => {
   return (
     <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
       <DndProvider backend={HTML5Backend}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/puns-of-anarchy/:playerId' element={<PunsOfAnarchy />} />
-          </Routes>
-        </BrowserRouter>
+        <h1>Lobby</h1>
+        <Lobby gameServer={server} lobbyServer={server} gameComponents={importedGames} />
       </DndProvider>
     </ConfigProvider>
   )
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import { Server, Origins } from 'boardgame.io/server';
 import PunsOfAnarchy from './PunsOfAnarchy/Game';
+import { PostgresStore } from "bgio-postgres";
+
+const db = new PostgresStore(process.env.DATABASE_URL || '');
 
 const server = Server({
   games: [PunsOfAnarchy],
   origins: [Origins.LOCALHOST],
+  db
 });
+
 
 const port = Number(process.env.PORT) || 8000;
 
